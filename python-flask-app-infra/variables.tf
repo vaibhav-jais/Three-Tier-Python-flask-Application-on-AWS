1a7e40ec2cfdace696d7595dad81ab7368dd25d6
vaibhav jaiswal <vaibhav572724@gmail.com>
2025-04-22 15:17:19 +0200

    unused variable 'vpc_name' causing error removed

--- python-flask-app-infra/variables.tf
+++ python-flask-app-infra/variables.tf
@@ -1,5 +1,4 @@
 variable "vpc_cidr" {}
-variable "vpc_name" {}
 variable "public_subnet_cidrs" { type = list(string) }
 variable "private_subnet_cidrs" { type = list(string) }
 variable "azs" { type = list(string) }
